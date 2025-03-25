c5261797306b82de466b022f55d41181a5103554
njeri.kahoro <njerii.kahoro@gmail.com>
2025-03-25 09:04:07 +0300

    initial commit

---
+++ src/basic_types.kt
@@ -0,0 +1,39 @@
+fun main() {
+    var customers = 10
+
+    customers = 8
+    customers = customers + 3
+    customers += 7
+    customers -= 3
+    customers *= 2
+    customers /= 3
+    println(customers)
+
+//    val d: Int
+//    d=3
+////
+//    val e: String
+//    e = "hello"
+//    println(d)
+//    println(e)
+
+    val a: Int
+    a = 1000
+    val b: String
+    b = "log message"
+    val c: Double
+    c = 3.14
+    val d: Long
+    d = 100_000_000_000_000
+    val e: Boolean
+    e = false
+    val f: Char
+    f = '\n'
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+
+}
---
+++ src/intro.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println("Hello, World!")
+    //Hello, World!
+
+    val customers = 10
+    println("There are $customers customers")
+    println("There are ${customers+1} customers")
+
+    val name = "Mary"
+    val age = 20
+    println("$name is $age years old")
+
+
+
+}
+
+
